84750eaeec6ea0b636b493456351d903f51373bc
vishalgoswami12 <vishalgoswami2250@gmail.com>
2023-07-11 12:10:20 +0530

    weather app with vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Weather from "./components/Weather.vue"
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Weather/>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
